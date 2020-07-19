0dc2a74f4d6961ad70f36cbf5c9cadb6d0eca00a
Patrick Kranz <patrick-kranz@live.de>
2020-07-19 17:12:19 +0200

    Fix padding for Bottom controls

---
+++ MusicPlayer/Converters/AddCoverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace MusicPlayer.Converters
+{
+    public class AddCoverter : IValueConverter
+    {
+        public int Precision { get; set; } = 2;
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+
+            if (value is double d)
+            {
+                var toadd = System.Convert.ToDouble(parameter);
+                d += toadd;
+                return d;
+            }
+            throw new NotImplementedException();
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
