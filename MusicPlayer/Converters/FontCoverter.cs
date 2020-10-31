e591be42e2c257f6c3e31c7287201787f31ceb71
Patrick Kranz <patrick-kranz@live.de>
2020-10-31 20:59:06 +0100

    Updated

--- MusicPlayer/Converters/FontCoverter.cs
+++ MusicPlayer/Converters/FontCoverter.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
 
 namespace MusicPlayer.Converters
 {
-
-    public class AddCoverter : IValueConverter
+    public class FontCoverter : IValueConverter
     {
-        public int Precision { get; set; } = 2;
 
         public object Convert(object value, Type targetType, object parameter, string language)
         {
 
-            if (value is double d)
+            if (value is char c)
             {
-                var toadd = System.Convert.ToDouble(parameter);
-                d += toadd;
-                return d;
+                if(c >= '\uE700'
+                    && c <= '\uF847')
+                {
+                    return new FontFamily("Segoe MDL2 Assets");
+                }
+                return null;
             }
             throw new NotImplementedException();
 
